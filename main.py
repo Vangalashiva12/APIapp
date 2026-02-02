533bda96c5a3b97a7953611c0c1f12c4dda2445f
Shiva Prasad Vangala <shivavangala19@gmail.com>
2026-02-02 15:26:37 -0500

    initial commit

---
+++ main.py
@@ -0,0 +1,16 @@
+from flask import Flask, render_template
+import pandas
+app = Flask(__name__)
+
+@app.route("/")
+def home():
+    return render_template("home.html")
+
+@app.route("/api/v1/<station>/<date>")
+def api(station, date):
+    df = pandas.read_csv()
+    temperature  = 23
+    return {"station": station, "date": date, "Temperature": temperature}
+
+if __name__ == "__main__":
+    app.run(debug=True)
